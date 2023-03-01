4e0f4a2d44e78b943497e9496ddf05c8a7bcefe3
jorgeaveraf <112434926+jorgeaveraf@users.noreply.github.com>
2023-03-01 09:28:15 -0600

    Add files via upload

---
+++ _1 Promedio alumno/src/Alumno.java
@@ -0,0 +1,32 @@
+import javax.swing.*;
+import java.util.Scanner;
+
+public class Alumno {
+    // Atributos
+    String Nombre;
+    Integer Promedio;
+    int EE;
+
+
+    /* Métodos*/
+//Pedir datos
+public void Pedir_Datos(){
+Nombre = String.valueOf(JOptionPane.showInputDialog("¿Cuál es tu nombre?"));
+EE = Integer.parseInt(JOptionPane.showInputDialog("¿Cuántas experiencias educativas cursaste?"));
+}
+    //Calcular el promedio
+public void Calificaciones(){
+    int i=0;
+    int j=0;
+    while(i < EE){
+        i++;
+        j+=Integer.parseInt(JOptionPane.showInputDialog("¿Cuál es la calificación de la experiencia educativa "+i+"?"));
+    }
+    Promedio=j/i;
+}
+    //Decir el promedio
+public void Enunciar(){
+    String.valueOf(JOptionPane.showInputDialog(Nombre+" el promedio de tus "+EE+" experiencias educativas es "+ Promedio.doubleValue()));
+}
+
+}
