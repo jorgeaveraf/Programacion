fb73c2e37bfe47a8293cadcf7e8da47604b41ea8
jorgeaveraf <112434926+jorgeaveraf@users.noreply.github.com>
2023-03-01 09:27:14 -0600

    Add files via upload

---
+++ Operaciones/src/Operaciones/Main.java
@@ -0,0 +1,13 @@
+package Operaciones;
+
+public class Main {
+    public static void main (String [] args){
+Operación Op1 = new Operación();
+Op1.Pedir_Numeros();
+Op1.Sumar();
+Op1.Restar();
+Op1.Multiplicar();
+Op1.Dividir();
+Op1.Resultados();
+    }
+}
