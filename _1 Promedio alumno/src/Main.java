2b09819a920870ed1d8cf4a67ba72157c9ccc92f
jorgeaveraf <112434926+jorgeaveraf@users.noreply.github.com>
2023-03-12 14:06:29 -0600

    Delete _1 Promedio alumno directory

--- _1 Promedio alumno/src/Main.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-
-public class Main {
-    public static void main(String[] args) {
-        Alumno alumno1 = new Alumno();
-        alumno1.Pedir_Datos();
-        alumno1.Calificaciones();
-        alumno1.Enunciar();    }
-}
